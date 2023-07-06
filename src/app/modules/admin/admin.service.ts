089b7845a7f3117e5b939fe47ac97f4f44ce2a0c
Arman-Kabir <armankabir8@gmail.com>
2023-07-07 03:24:58 +0600

    password matching done

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -3,37 +3,41 @@ import { IAdmin } from "./admin.interface";
 import { Admin } from "./admin.model";
 import bcrypt from 'bcrypt';
 import { generateAdminId } from "./admin.utils";
+import ApiError from "../../../errors/ApiError";
 
 
-export const createAdminService = async(payload: any): Promise<IAdmin> => {
+export const createAdminService = async (payload: any): Promise<IAdmin> => {
   // console.log(payload);
-  // const { password } = payload;
   // Hash password
   // payload.password = await bcrypt.hash(payload.password, Number(config.bcrypt_salt_rounds))
-  payload.id= await generateAdminId();
+  payload.id = await generateAdminId();
   // console.log(payload.id);
   const results = await Admin.create(payload);
-  
+
   return results;
-
-  // console.log(password);
-
-
-  // if(!results){
-  //     throw new ApiError(400,'Failed to create user');
-  // }
 };
 
 
-export const loginAdminService = (payload: any): Promise<IAdmin> => {
-  // const results = Admin.create(payload);
+export const loginAdminService = async (payload: any) => {
+  const { id, password } = payload;
   console.log(payload);
-  // console.log(results);
 
-  // if(!results){
-  //     throw new ApiError(400,'Failed to create user');
-  // }
-  // return results;
+  const isAdminExist = await Admin.findOne({ id }, { id: 1, password: 1 });
+  console.log(isAdminExist);
+
+  if(!isAdminExist){
+    throw new ApiError(400,'Admin not exist');
+  }
+  
+  // Match password
+  const isPasswordMatched = await bcrypt.compare(password,isAdminExist?.password);
+  console.log(isPasswordMatched);
+
+  if(!isPasswordMatched){
+    throw new ApiError(500,'Password not matched');
+  }
+
+  // create access token
 };
 
 
