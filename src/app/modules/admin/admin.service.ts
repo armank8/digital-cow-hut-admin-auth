83f8d9c056e46120bf5d6977f3dd85574e25d33f
Arman-Kabir <armankabir8@gmail.com>
2023-07-06 23:42:48 +0600

    password hashing done

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,19 +1,23 @@
+import config from "../../../config";
 import { IAdmin } from "./admin.interface";
 import { Admin } from "./admin.model";
 import bcrypt from 'bcrypt';
 
 
-export const createAdminService = (payload: any): Promise<IAdmin> => {
-  console.log(payload);
-  // const results = Admin.create(payload);
-
-  // console.log(results);
+export const createAdminService = async(payload: any): Promise<IAdmin> => {
+  // console.log(payload);
+  // const { password } = payload;
   // Hash password
-  
+  payload.password = await bcrypt.hash(payload.password, Number(config.bcrypt_salt_rounds))
+  const results = await Admin.create(payload);
+
+  // console.log(password);
+
+
   // if(!results){
   //     throw new ApiError(400,'Failed to create user');
   // }
-  // return results;
+  return results;
 };
 export const loginAdminService = (payload: any): Promise<IAdmin> => {
   const results = Admin.create(payload);
