13c256c2546de1a23970451850e314eae7ec5da8
Arman-Kabir <armankabir8@gmail.com>
2023-07-07 03:56:08 +0600

    env added

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -4,7 +4,7 @@ import { Admin } from "./admin.model";
 import bcrypt from 'bcrypt';
 import { generateAdminId } from "./admin.utils";
 import ApiError from "../../../errors/ApiError";
-
+var jwt = require('jsonwebtoken');
 
 export const createAdminService = async (payload: any): Promise<IAdmin> => {
   // console.log(payload);
@@ -22,22 +22,29 @@ export const loginAdminService = async (payload: any) => {
   const { id, password } = payload;
   console.log(payload);
 
-  const isAdminExist = await Admin.findOne({ id }, { id: 1, password: 1 });
+  const isAdminExist = await Admin.findOne({ id }, { id: 1, password: 1,role:1 });
   console.log(isAdminExist);
 
-  if(!isAdminExist){
-    throw new ApiError(400,'Admin not exist');
+  if (!isAdminExist) {
+    throw new ApiError(400, 'Admin not exist');
   }
-  
+
   // Match password
-  const isPasswordMatched = await bcrypt.compare(password,isAdminExist?.password);
+  const isPasswordMatched = await bcrypt.compare(password, isAdminExist?.password);
   console.log(isPasswordMatched);
 
-  if(!isPasswordMatched){
-    throw new ApiError(500,'Password not matched');
+  if (!isPasswordMatched) {
+    throw new ApiError(500, 'Password not matched');
   }
 
-  // create access token
+  // create access token & refresh token
+  const accessToken = jwt.sign({
+    id:isAdminExist?.id,
+    role:isAdminExist?.role
+  },);
+
+
+
 };
 
 
