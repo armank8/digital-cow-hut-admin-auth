190f7f48ccb51927b51ca33f3c54922313c5bd44
Arman-Kabir <armankabir8@gmail.com>
2023-07-06 14:16:37 +0600

    bcrypt half done

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,15 +1,19 @@
 import { IAdmin } from "./admin.interface";
 import { Admin } from "./admin.model";
+import bcrypt from 'bcrypt';
 
 
 export const createAdminService = (payload: any): Promise<IAdmin> => {
-  const results = Admin.create(payload);
-  // console.log(results);
+  console.log(payload);
+  // const results = Admin.create(payload);
 
+  // console.log(results);
+  // Hash password
+  
   // if(!results){
   //     throw new ApiError(400,'Failed to create user');
   // }
-  return results;
+  // return results;
 };
 export const loginAdminService = (payload: any): Promise<IAdmin> => {
   const results = Admin.create(payload);
