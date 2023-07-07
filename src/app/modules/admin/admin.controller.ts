96bf8a460f6505ce710fbd2d6d08b99e535a46a9
Arman-Kabir <armankabir8@gmail.com>
2023-07-07 16:20:39 +0600

    refresh token stored in cookie

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { createAdminService, loginAdminService } from './admin.service';
+import config from '../../../config';
 
 
 
@@ -7,6 +8,7 @@ export const createAdmin = async (req: Request, res: Response, next: NextFunctio
   try {
     const userData = req.body;
     const result = await createAdminService(userData);
+    console.log(req.cookies, 'cookie');
     res.status(200).json({
       success: true,
       statusCode: 200,
@@ -20,13 +22,30 @@ export const createAdmin = async (req: Request, res: Response, next: NextFunctio
 
 export const loginAdmin = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const userData = req.body;
-    const result = await loginAdminService(userData);
+    const adminData = req.body;
+    const result = await loginAdminService(adminData);
+
+    const { refreshToken, ...others } = result;
+
+    // set refresh token into cookie
+    const cookieOptions = {
+      secure: config.env === 'production',
+      httpOnly: true
+    };
+    res.cookie('refreshToken', result.refreshToken, cookieOptions);
+
+    //delete refresh token
+    // delete result.refreshToken 
+    if ('refreshToken' in result) {
+      delete result.refreshToken
+    }
+
     res.status(200).json({
       success: true,
       statusCode: 200,
       message: "User logged in successfully",
-      data: result
+      // data: result
+      data: others
     })
   } catch (err) {
     next(err);
--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -4,6 +4,7 @@ import { Admin } from "./admin.model";
 import bcrypt from 'bcrypt';
 import { generateAdminId } from "./admin.utils";
 import ApiError from "../../../errors/ApiError";
+import { Secret } from "jsonwebtoken";
 var jwt = require('jsonwebtoken');
 
 export const createAdminService = async (payload: any): Promise<IAdmin> => {
@@ -20,10 +21,10 @@ export const createAdminService = async (payload: any): Promise<IAdmin> => {
 
 export const loginAdminService = async (payload: any) => {
   const { id, password } = payload;
-  console.log(payload);
+  // console.log(payload);
 
-  const isAdminExist = await Admin.findOne({ id }, { id: 1, password: 1,role:1 });
-  console.log(isAdminExist);
+  const isAdminExist = await Admin.findOne({ id }, { id: 1, password: 1, role: 1 });
+  // console.log(isAdminExist);
 
   if (!isAdminExist) {
     throw new ApiError(400, 'Admin not exist');
@@ -31,7 +32,7 @@ export const loginAdminService = async (payload: any) => {
 
   // Match password
   const isPasswordMatched = await bcrypt.compare(password, isAdminExist?.password);
-  console.log(isPasswordMatched);
+  // console.log(isPasswordMatched);
 
   if (!isPasswordMatched) {
     throw new ApiError(500, 'Password not matched');
@@ -39,11 +40,23 @@ export const loginAdminService = async (payload: any) => {
 
   // create access token & refresh token
   const accessToken = jwt.sign({
-    id:isAdminExist?.id,
-    role:isAdminExist?.role
-  },);
+    id: isAdminExist?.id,
+    role: isAdminExist?.role
+  }, config.jwt.secret as Secret, {
+    expiresIn: config.jwt.expires_in
+  });
 
+  const refreshToken = jwt.sign({
+    id: isAdminExist?.id,
+    role: isAdminExist?.role
+  }, config.jwt.refresh_secret as Secret, {
+    expiresIn: config.jwt.refresh_expires_in
+  });
 
+  return {
+    accessToken,
+    refreshToken
+  }
 
 };
 
