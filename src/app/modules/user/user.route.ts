391f29070050af114e19b4f233801d2787051b0e
Arman-Kabir <armankabir8@gmail.com>
2023-07-09 23:36:44 +0600

    user roles

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { deleteUser, getSingleUser, getUsers, updateUser } from "./user.controller";
+import { auth } from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enums/user";
 
 const router = express.Router();
 
-router.get("/", getUsers);
-router.get("/:id", getSingleUser);
-router.patch("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/",auth(ENUM_USER_ROLE.ADMIN), getUsers);
+router.get("/:id",auth(ENUM_USER_ROLE.ADMIN), getSingleUser);
+router.patch("/:id",auth(ENUM_USER_ROLE.ADMIN), updateUser);
+router.delete("/:id",auth(ENUM_USER_ROLE.ADMIN), deleteUser);
 
 export default router;
