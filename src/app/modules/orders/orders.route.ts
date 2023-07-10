ff0f50af1092bc9aa4ad40d7bf4e3221dc9f90fe
Arman-Kabir <armankabir8@gmail.com>
2023-07-10 14:24:49 +0600

    create orders authorized

--- src/app/modules/orders/orders.route.ts
+++ src/app/modules/orders/orders.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { createOrder, getOrders } from "./orders.controller";
+import { auth } from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enums/user";
 
 const router = express.Router();
 
-router.post("/", createOrder);
+router.post("/",auth(ENUM_USER_ROLE.BUYER), createOrder);
 router.get("/", getOrders);
 
 
